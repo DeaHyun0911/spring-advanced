a3dd9af5a43918f41fe75ecd23a0ff858f72ef76
DeaHyun0911 <eogus0911@gmail.com>
2025-01-02 19:53:53 +0900

    Lv 5-1 : PasswordEncoderTest
    - matches 파라미터 순서 변경

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -3,26 +3,27 @@ package org.example.expert.config;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 class PasswordEncoderTest {
 
-    @InjectMocks
-    private PasswordEncoder passwordEncoder;
+	@InjectMocks
+	PasswordEncoder passwordEncoder;
 
-    @Test
-    void matches_메서드가_정상적으로_동작한다() {
-        // given
-        String rawPassword = "testPassword";
-        String encodedPassword = passwordEncoder.encode(rawPassword);
+	@Test
+	void matches_메서드가_정상적으로_동작한다() {
+		// given
+		String rawPassword = "testPassword";
+		String encodedPassword = passwordEncoder.encode(rawPassword);
 
-        // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+		// when
+		boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
 
-        // then
-        assertTrue(matches);
-    }
+		// then
+		assertTrue(matches);
+	}
 }
